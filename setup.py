ae27dc5a2461940bc69e1880e9babb6bcd43aaf1
es3n1n <me@es3n.in>
2024-09-23 13:50:14 +0200

    ci: publish package on tag ref

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='re-unplayplay',
-    version='1.0.0a',
+    version='1.1.0',
     author='es3n1n',
     description='A rewritten version of unplayplay',
     long_description='https://github.com/es3n1n/re-unplayplay',
