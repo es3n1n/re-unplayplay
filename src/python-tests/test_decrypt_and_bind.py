8ba133e719b6fe6b8748ff66850013e8e985cf63
Arsenii es3n1n <me@es3n.in>
2024-09-25 12:11:52 +0200

    ci(py): add cibuildwheel

--- src/python-tests/test_decrypt_and_bind.py
+++ src/python-tests/test_decrypt_and_bind.py
@@ -2,4 +2,7 @@ from re_unplayplay import decrypt_and_bind_key
 
 
 def test_bind_key() -> None:
-    assert decrypt_and_bind_key(bytes.fromhex('01'), '01').hex() == 'f3a37e28d15bc5e8151d413eb36a59b5'
+    assert (
+        decrypt_and_bind_key(bytes.fromhex('01'), '01').hex()
+        == 'f3a37e28d15bc5e8151d413eb36a59b5'
+    )
