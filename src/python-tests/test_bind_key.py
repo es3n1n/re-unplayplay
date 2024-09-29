023214da31b77690594bc59a3d6038d6f737ec5f
Arsenii es3n1n <me@es3n.in>
2024-09-29 21:59:58 +0200

    test(py): fix python bind test

--- src/python-tests/test_bind_key.py
+++ src/python-tests/test_bind_key.py
@@ -3,4 +3,4 @@ from re_unplayplay import decrypt_key, bind_key
 
 def test_bind_key() -> None:
     decrypted = decrypt_key(bytes.fromhex('01'))
-    assert bind_key(decrypted, '01').hex() == 'E32698DCAFABA7529A223C8E34EC08CC'
+    assert bind_key(decrypted, '01').hex() == 'e32698dcafaba7529a223c8e34ec08cc'
