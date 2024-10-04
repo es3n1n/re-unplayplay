a4824a46d09bdd54c137e00ceef28f36791012a4
Arsenii es3n1n <me@es3n.in>
2024-10-04 13:26:39 +0200

    chore: remove unused js/ts stuff

--- tests/js/unit.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const binding_1 = require("binding");
-describe('binding', () => {
-    test('hello function returns expected string', () => {
-        const result = (0, binding_1.hello)();
-        expect(result).toBe('world'); // Replace 'world' with the expected return value
-    });
-});
