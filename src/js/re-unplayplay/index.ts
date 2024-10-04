508beecbd8f16e1f260d2b0c65bcb140bcae34b3
Arsenii es3n1n <me@es3n.in>
2024-10-04 18:26:36 +0200

    fix(js): fix module resolution in production

--- src/js/re-unplayplay/index.ts
+++ src/js/re-unplayplay/index.ts
@@ -1,7 +1,24 @@
-const _re_unplayplay = require("pkg-prebuilds")(
-    __dirname + '../../../../',
-    require("../../../binding-options.js")
-);
+let _re_unplayplay: any;
+
+try {
+    /// Production:
+    /// dist/src/js/re-unplayplay/index.ts
+    _re_unplayplay = require("pkg-prebuilds")(
+        __dirname + '../../../../../',
+        require(__dirname + "../../../../../binding-options.js"),
+    );
+} catch(err) {
+    /// Development:
+    /// src/js/re-unplayplay/index.ts
+    _re_unplayplay = require("pkg-prebuilds")(
+        __dirname + '../../../../',
+        require(__dirname + "../../../../binding-options.js"),
+    );
+}
+
+if (!_re_unplayplay) {
+    throw new Error("_re_unplayplay was not initialized successfully");
+}
 
 export function decryptKey(encryptedKey: Buffer): Buffer {
     return _re_unplayplay.decryptKey(encryptedKey);
