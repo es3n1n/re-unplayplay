fe229c31fbebf9e9ff696ce2dead3a778c6911fb
Arsenii es3n1n <me@es3n.in>
2024-11-13 13:00:19 +0100

    refactor: update keys

--- tests/python/test_unit.py
+++ tests/python/test_unit.py
@@ -4,5 +4,5 @@ from re_unplayplay import decrypt_and_bind_key
 def test_decrypt_and_bind_key() -> None:
     assert (
         decrypt_and_bind_key(bytes.fromhex('01'), '01').hex().upper()
-        == 'E6B81C1574CFE0E1A4D78D042E290D29'
+        == '523FF1673A7A66E4BA8435B18F69D6D6'
     )
