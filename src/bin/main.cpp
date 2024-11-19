0453115eeb09469d3cde9aa2da04cc176ad43e5e
Arsenii es3n1n <me@es3n.in>
2024-11-19 19:01:59 +0100

    refactor(cli): move stuff around

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,35 +1,40 @@
+#include "unplayplay.hpp"
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include "unplayplay.hpp"
 
-int main(const int argc, const char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "usage: " << argv[0] << " {file_id} {encrypted_key}" << std::endl;
-        return 1;
-    }
-
-    std::string arg1(argv[1]);
-    if (arg1 == "token") {
-        const auto token = unplayplay::get_token();
-
-        for (std::size_t i = 0; i < token.size(); ++i) {
-            std::cout << std::setw(2) << std::setfill('0') << std::hex
-                      << static_cast<int>(token[i]);
+namespace {
+    template <std::size_t N>
+    void to_stdout(const unplayplay::util::ByteBuffer<N>& value) {
+        for (std::size_t i = 0; i < value.size(); ++i) {
+            std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(value[i]);
         }
         std::cout << std::endl;
+    }
 
+    void usage(const char* argv[]) {
+        std::cerr << "Usage: " << argv[0] << " <command> [args...]" << std::endl
+                  << "Commands:" << std::endl
+                  << "  token                          Display linked token" << std::endl
+                  << "  <file_id_hex> <enc_key_hex>    Decrypt and bind key for specified file" << std::endl;
+    }
+} // namespace
+
+int main(const int argc, const char* argv[]) {
+    if (argc == 2 && std::string_view{argv[1]} == "token") {
+        to_stdout(unplayplay::get_token());
         return 0;
+    }
+
+    if (argc != 3) {
+        usage(argv);
+        return 1;
     }
 
     const auto file_id = unplayplay::FileId(argv[1]);
     const auto encrypted_key = unplayplay::Key(argv[2]);
     const auto result = unplayplay::decrypt_and_bind_key(encrypted_key, file_id);
 
-    for (std::size_t i = 0; i < result.size(); ++i) {
-        std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(result[i]);
-    }
-    std::cout << std::endl;
-
+    to_stdout(result);
     return 0;
 }
