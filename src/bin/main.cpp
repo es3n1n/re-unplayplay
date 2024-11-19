bf1437bc73667c6634b18ed2eb50c272c5508090
xiaomengxin <xibaole2333@gmail.com>
2024-11-20 01:46:52 +0800

    add `token` command to output token as hex

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,12 +1,25 @@
 #include <iomanip>
 #include <iostream>
-
-#include <unplayplay.hpp>
+#include <string>
+#include "unplayplay.hpp"
 
 int main(const int argc, const char* argv[]) {
     if (argc < 2) {
         std::cerr << "usage: " << argv[0] << " {file_id} {encrypted_key}" << std::endl;
         return 1;
+    }
+
+    std::string arg1(argv[1]);
+    if (arg1 == "token") {
+        const auto token = unplayplay::get_token();
+
+        for (std::size_t i = 0; i < token.size(); ++i) {
+            std::cout << std::setw(2) << std::setfill('0') << std::hex
+                      << static_cast<int>(token[i]);
+        }
+        std::cout << std::endl;
+
+        return 0;
     }
 
     const auto file_id = unplayplay::FileId(argv[1]);
